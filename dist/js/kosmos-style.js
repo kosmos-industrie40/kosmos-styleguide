362615de4294f1a34067ef1d369960bbbefeb491
Elena Schediwie <elena.schediwie@inovex.de>
2021-12-01 09:12:51 +0100

    Renaming mostly

--- dist/js/kosmos-style.js
+++ dist/js/kosmos-style.js
@@ -4,13 +4,25 @@
 
 
 // toggle sidebar menu
-const MAIN_CONTAINER = '#main_container';
-const COLLAPSED_CLASS = 'sidebar_collapsed'; // no . needed here
-const TOGGLE_BUTTON = '#menu_toggle';
+const MAIN_CONTAINER = '#main_container'
+const COLLAPSED_CLASS = 'sidebar_collapsed' // no . needed here
+const TOGGLE_BUTTON = '#menu_toggle'
+let collapsed_state = false
 
 $(document).ready( function() {
   $(TOGGLE_BUTTON).on('click', function() {
     console.info('Toggling Sidebar');
     $(MAIN_CONTAINER).toggleClass(COLLAPSED_CLASS);
+
+    // switch out the icon
+    if(collapsed_state === false){
+      $(TOGGLE_BUTTON).find('.fa').addClass('fa-chevron-right').removeClass('fa-chevron-left')
+      collapsed_state = true
+    }
+    else {
+      $(TOGGLE_BUTTON).find('.fa').addClass('fa-chevron-left').removeClass('fa-chevron-right')
+      collapsed_state = false
+    }
+
   });
 });
